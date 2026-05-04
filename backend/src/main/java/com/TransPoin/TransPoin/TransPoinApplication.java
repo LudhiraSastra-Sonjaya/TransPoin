282f315ffef1f0c36d91a1158812a3e94f7f6bf5
Azizan Rizqan <azizanrizqan.ar@gmail.com>
2026-05-04 15:09:42 +0700

    init Project Pertama

---
+++ backend/src/main/java/com/TransPoin/TransPoin/TransPoinApplication.java
@@ -0,0 +1,13 @@
+package com.TransPoin.TransPoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransPoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransPoinApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/TransPoin/TransPoin/TransPoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TransPoin.TransPoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransPoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
